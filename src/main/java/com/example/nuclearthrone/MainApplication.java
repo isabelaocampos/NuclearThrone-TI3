5ecc4906a30c7e56968cee463c896bcdd510bbca
unknown <1109115195@u.icesi.edu.co>
2023-05-27 14:14:11 -0500

    Base del proyecto

---
+++ src/main/java/com/example/nuclearthrone/MainApplication.java
@@ -0,0 +1,31 @@
+package com.example.nuclearthrone;
+
+import com.example.nuclearthrone.control.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        Parent parent = fxmlLoader.load();
+        Scene scene = new Scene(parent, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.setOnCloseRequest(windowEvent -> {
+            MainController controller = fxmlLoader.getController();
+            controller.setRunning(false);
+
+        });
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
