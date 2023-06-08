f8079ec2e69261c744107d61bb0a5eef23e699e6
Isabella <isabelaocampo08@gmail.com>
2023-06-08 10:11:36 -0500

    Added for maps

--- src/main/java/com/example/nuclearthrone/control/MainController.java
+++ src/main/java/com/example/nuclearthrone/control/MainController.java
@@ -1,10 +1,13 @@
 package com.example.nuclearthrone.control;
+import com.example.nuclearthrone.model.Level;
+import com.example.nuclearthrone.model.Player;
 import com.example.nuclearthrone.screens.BaseScreen;
 import com.example.nuclearthrone.screens.ScreenA;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -12,14 +15,26 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
+    public static final String Main_Resources_Path = System.getProperty("user.dir") + "\\src\\main\\resources";
+    public static final String Main_Resources_Images = Main_Resources_Path + "";
+
+    public static final String Main_Resources_Audio_Path = Main_Resources_Path + "";
+    public static final String Collision_Pack = Main_Resources_Path + "";
+    public static int level = 0;
+    private ArrayList<Level> levels;
+    private Player players;
     @FXML
     private Canvas canvas;
+
+    @FXML
+    private GraphicsContext graphics;
 
     private boolean isRunning;
 
     public static int SCREEN = 0;
 
     private ArrayList<BaseScreen> screens;
+    public static boolean reset;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -71,5 +86,20 @@ public class MainController implements Initializable {
         }catch (InterruptedException e){
             e.printStackTrace();
         }
+    }
+
+    private void start(){
+        graphics = canvas.getGraphicsContext2D();
+        reset = false;
+        isRunning = true;
+        levels = new ArrayList<>();
+        //players = new Player();
+        canvas.setFocusTraversable(true);
+        //printInCanvas();
+        //levels.add(new Level(44,30,canvas,"\\", players, 1));
+        //levels.add(new Level(44,30,canvas,"\\", players, 2));
+        //levels.add(new Level(44,30,canvas,"\\", players, 3));
+        initEvents();
+
     }
 }
