ca00f7dd18094c56642051cef19c6d3fa0560106
Valentina <valen201611@gmail.com>
2023-06-09 09:12:15 -0500

    h

--- src/main/java/com/example/nuclearthrone/model/entity/important/Player.java
+++ src/main/java/com/example/nuclearthrone/model/entity/important/Player.java
@@ -5,6 +5,7 @@ import javafx.animation.Animation;
 import javafx.animation.Timeline;
 
 import com.example.nuclearthrone.model.entity.util.Vector;
+
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ProgressBar;
@@ -19,7 +20,6 @@ public class Player {
     //** */
     public static final int HEALTH = 100;
     private static Player instance;
-
     private static Timeline animationPlayer;
     public boolean isAlive;
     //** */
@@ -31,6 +31,8 @@ public class Player {
     private ArrayList<Image> idleImages;
     private ArrayList<Image> runImages;
     private ArrayList<Image> attackImages;
+
+    private ArrayList<Image> dieImages;
 
     // referencias espaciales
     private double posX;
@@ -48,6 +50,8 @@ public class Player {
     private boolean rightPressed;
     private boolean isAttacking;
 
+
+
     //
     private boolean isFacingRight = true;
     private boolean isMoving;
@@ -59,44 +63,53 @@ public class Player {
 
     public static final double WIDTH = 50;
     public static final double HEIGHT = 50;
-    public static Player getInstance(){
-        if(instance== null){
-            instance = new Player(200,70,WIDTH,HEIGHT);
-            
+
+    public static Player getInstance() {
+        if (instance == null) {
+            instance = new Player(200, 70, WIDTH, HEIGHT);
+
         }
         return instance;
     }
-    
-    public Player(double x, double y, double width, double height){
+
+    public Player(double x, double y, double width, double height) {
         super();
         boolean isAlive = true;
     }
 
-
-    public Player(Canvas canvas){
+    public Player(Canvas canvas) {
         this.state = 0;
         this.canvas = canvas;
         this.graphicsContext = canvas.getGraphicsContext2D();
+        this.isAlive = true;
+
 
         this.position = new Vector(100, 100);
 
         idleImages = new ArrayList<>();
         runImages = new ArrayList<>();
         attackImages = new ArrayList<>();
+        dieImages = new ArrayList<>();
 
-        for(int i = 1; i <= 3; i++){
-            Image image = MainApplication.getImage("animations/hero/idle/idle_0"+i+".png");
+
+        for (int i = 1; i <= 3; i++) {
+            Image image = MainApplication.getImage("animations/hero/idle/idle_0" + i + ".png");
             idleImages.add(image);
         }
 
-        for(int i = 1; i <= 5; i++){
-            Image image = MainApplication.getImage("animations/hero/run/run_0" +i+".png");
+        for (int i = 1; i <= 5; i++) {
+            Image image = MainApplication.getImage("animations/hero/run/run_0" + i + ".png");
             runImages.add(image);
         }
 
-        for(int i = 1; i <= 4; i++){
-            Image image = MainApplication.getImage("animations/hero/shoot/shoot_0"+i+".png");
+        for (int i = 1; i <= 4; i++) {
+            Image image = MainApplication.getImage("animations/hero/shoot/shoot_0" + i + ".png");
             attackImages.add(image);
+        }
+
+        for (int i = 1; i <= 8; i++) {
+            Image image = MainApplication.getImage("animations/hero/dead/dead_0" + i + ".png");
+            dieImages.add(image);
         }
 
     }
@@ -105,29 +118,37 @@ public class Player {
     public void paint() {
         onMove();
 
-        if (state == 0) {
-            double width = isFacingRight ? 50 : -50;
-            double xOffset = isFacingRight ? -25 : 25;
+        if (isAlive) {
 
-            graphicsContext.drawImage(idleImages.get(frame % 3), position.getX() + xOffset, position.getY() - 25, width, 50);
-        } else if (state == 1) {
-            double width = isFacingRight ? 60 : -60;
-            double xOffset = isFacingRight ? -30 : 30;
+            if (state == 0) {
+                double width = isFacingRight ? 50 : -50;
+                double xOffset = isFacingRight ? -25 : 25;
 
-            graphicsContext.drawImage(runImages.get(frame % 5), position.getX() + xOffset, position.getY() - 25, width, 50);
-        } else if (state == 2) {
-            double width = isFacingRight ? 70 : -70;
-            double xOffset = isFacingRight ? -35 : 35;
+                graphicsContext.drawImage(idleImages.get(frame % 3), position.getX() + xOffset, position.getY() - 25, width, 50);
+            } else if (state == 1) {
+                double width = isFacingRight ? 60 : -60;
+                double xOffset = isFacingRight ? -30 : 30;
 
-            graphicsContext.drawImage(attackImages.get(frame % 4), position.getX() + xOffset, position.getY() - 25, width, 50);
+                graphicsContext.drawImage(runImages.get(frame % 5), position.getX() + xOffset, position.getY() - 25, width, 50);
+            } else if (state == 2) {
+                double width = isFacingRight ? 70 : -70;
+                double xOffset = isFacingRight ? -35 : 35;
+
+                graphicsContext.drawImage(attackImages.get(frame % 4), position.getX() + xOffset, position.getY() - 25, width, 50);
+            }
+
+        } else {
+            if (state == 3) {
+                graphicsContext.drawImage(dieImages.get(frame % 8), position.getX(), position.getY());
+
+            }
         }
 
-        frame++;
     }
 
 
-    public void onKeyPressed(KeyEvent event){
-        switch (event.getCode()){
+    public void onKeyPressed(KeyEvent event) {
+        switch (event.getCode()) {
             case W:
                 state = 1;
                 upPressed = true;
@@ -147,9 +168,9 @@ public class Player {
         }
     }
 
-    public void onKeyReleased(KeyEvent event){
+    public void onKeyReleased(KeyEvent event) {
         state = 0;
-        switch (event.getCode()){
+        switch (event.getCode()) {
             case W:
                 upPressed = false;
                 break;
@@ -165,17 +186,17 @@ public class Player {
         }
     }
 
-    public void onMove(){
-        if (upPressed){
+    public void onMove() {
+        if (upPressed) {
             position.setY(position.getY() - 10);
         }
-        if (downPressed){
+        if (downPressed) {
             position.setY(position.getY() + 10);
         }
-        if (leftPressed){
+        if (leftPressed) {
             position.setX(position.getX() - 10);
         }
-        if (rightPressed){
+        if (rightPressed) {
             position.setX(position.getX() + 10);
         }
     }
@@ -184,37 +205,39 @@ public class Player {
         return position;
     }
 
-    public boolean isFacingRight() {
-        return isFacingRight;
-    }
 
     public void setFacingRight(boolean facingRight) {
         isFacingRight = facingRight;
     }
-/*
-    private static void updateLifeBar() {
-        int nHearts = hearts.length;
-        for (int i = 0; i < nHearts; i++) {
-            if((i+1)*30 < instance.health){
-                hearts[i].setImage(new Image(MainMenu.getFile("windows/full-heart.png").getPath()));
-            }else{
-                hearts[i].setImage(new Image(MainMenu.getFile("windows/empty-heart.png").getPath()));
-            }
-        }
+
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
     }
 
-*/
-    public static void resetAvatar(){
+
+    public static void resetAvatar() {
         instance.stopAnimation();
         instance = null;
     }
-    
+
     public void stopAnimation() {
         animationPlayer.stop();
     }
+
     public void startAnimation() {
         animationPlayer.setCycleCount(Animation.INDEFINITE);
         animationPlayer.play();
+
     }
 
+        public void remove(){
+            setAlive(false);
+            state = 3;  // Establecer el estado en 3 para mostrar la animación de muerte
+            frame = 0;  // Reiniciar el contador de frames para la animación de muerte
+            //aaaaa
+        }
+
+
 }
+
